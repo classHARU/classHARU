cd0242e7c6ab70761d7cc48996fb01cfea0195a4
kyunglyn <amy3249@naver.com>
2023-03-06 17:14:55 +0900

    230306_틀

--- ClassHARU/src/main/java/com/sist/web/MainController.java
+++ ClassHARU/src/main/java/com/sist/web/MainController.java
@@ -1,10 +1,12 @@
 package com.sist.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-	public static void main(String[] args) {
-		System.out.println("dd");
+	@GetMapping("main/main.do")
+	public String main_main() {
+		return "main";
 	}
 }
